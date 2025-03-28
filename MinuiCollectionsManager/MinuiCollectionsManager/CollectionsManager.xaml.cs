08801aff9c2ca7057d34ed5921a3f3d25a5ed607
frichard0 <frichard@karibu-gestion.fr>
2025-03-28 08:40:57 +0100

    Added home page

---
+++ MinuiCollectionsManager/MinuiCollectionsManager/CollectionsManager.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace MinuiCollectionsManager;
+
+public partial class CollectionsManager : Page
+{
+    private string _folderPath;
+    public CollectionsManager(string folderPath)
+    {
+        InitializeComponent();
+        _folderPath = folderPath;
+    }
+}
---
+++ MinuiCollectionsManager/MinuiCollectionsManager/Home.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
+namespace MinuiCollectionsManager;
+
+public partial class Home : Page
+{
+    public Home()
+    {
+        InitializeComponent();
+    }
+    
+    private void LoadFolder_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new System.Windows.Forms.FolderBrowserDialog();
+        if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        {
+            string folderPath = dialog.SelectedPath;
+            
+            NavigationService.Navigate(new CollectionsManager(folderPath));
+        }
+    }
+}
